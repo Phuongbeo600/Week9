2daf8cd55852fcc74e1dc58cad6370a15c98c41c
Phuong <email@cua.m>
2026-05-05 13:31:45 +0700

    sửa theo bài số 8

---
+++ b6/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Lớp Main để chạy chương trình.
+ */
+public class Main {
+    /**
+     * Hàm main chính.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
